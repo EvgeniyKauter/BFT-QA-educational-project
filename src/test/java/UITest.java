95c562833dec0ca3b9c071449b533c3b86660b50
EvgeniyKauter <evgeniykauter@mail.ru>
2025-11-30 18:18:54 +0300

    change uitest.java

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
@@ -6,23 +7,28 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class UITest {
 
-    SearchPage ribalkaSearch = new SearchPage();
-    SearchPage searchRibalka = new SearchPage();
-    @Test
-    void test1() {
-
-        ribalkaSearch.openPage("https://ya.ru/")
-                .setInputWord("рыбалка")
-                .pressEnter()
-                .toBeVisible($(By.xpath("/html/body/div[1]/div[1]/header/form/div[1]/a/svg/path[1]")));
-
-    }
+//    System.out.println(null); пример где на 0 делить нельзя
+//    Assertions.assertTrue(false);
 
     @Test
-    void test2(){
+    void exception() {
 
-        searchRibalka.openPage("https://ya.ru/")
-                .wait(3000)
-                .setInputWord("рыбалка");
+        int a = 5;
+        int b = 5;
+        int c = a - b;
+
+        try {
+            System.out.println(a / c);
+        }
+        catch (Exception e) {
+            System.out.println("Нельзя делить на 0");
+        }
+
+        try {
+            Assertions.assertTrue(false);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
     }
+
 }
