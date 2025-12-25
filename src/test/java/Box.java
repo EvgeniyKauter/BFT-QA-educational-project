6e4415ccbdd4e0e801da05ed48b5b8d16e968563
EvgeniyKauter <evgeniykauter@mail.ru>
2025-12-25 16:41:34 +0300

    Homework 14

---
+++ src/test/java/Box.java
@@ -0,0 +1,25 @@
+public class Box {
+
+    private double width;
+    private double height;
+    private double depth;
+
+    public Box (double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "width=" + width +
+                ", height=" + height +
+                ", depth=" + depth +
+                '}';
+    }
+}
